b601f46dfee48d6c02884ebec371d63997e3c958
cipher0ne <199516992+loonhaar@users.noreply.github.com>
2026-05-09 00:27:21 +0200

    Feature quitting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
-fn main() {
-	println!("Hello, world!");
+use std::io::{self, Write};
+
+fn main() -> io::Result<()> {
+	let result = App::default().run();
+	result
+}
+
+#[derive(Debug, Default)]
+struct App {
+	buffer: Vec<String>,
+	caret_address: usize,
+	modified_buffer: bool,
+	mode: Mode,
+}
+
+#[derive(Debug, Default)]
+enum Mode {
+	#[default]
+	Cmd,
+	Edit,
+}
+
+impl App {
+	fn run(mut self) -> io::Result<()> {
+		let mut line = String::new();
+
+		loop {
+			line.clear();
+
+			if io::stdin().read_line(&mut line)? == 0 {
+				break;
+			}
+
+			let input = line.trim();
+
+			match self.mode {
+				Mode::Cmd => {
+					if self.handle_cmd(input) {
+						break;
+					}
+				}
+				_ => {}
+			}
+		}
+
+		Ok(())
+	}
+
+	fn handle_cmd(&mut self, input: &str) -> bool {
+		match input {
+			"q" => true,
+			_ => {
+				println!("?");
+				false
+			}
+		}
+	}
+
+	fn handle_edit() {}
 }
