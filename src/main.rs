d87c136089529f2f15adaee59e5bd3575716c8ce
cipher0ne <199516992+loonhaar@users.noreply.github.com>
2026-05-09 02:32:24 +0200

    Fix list command; Add delete command

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() -> io::Result<()> {
 #[derive(Debug, Default)]
 struct App {
 	buffer: Vec<String>,
-	caret_address: usize,
+	current_address: usize,
 	modified_buffer: bool,
 	mode: Mode,
 }
@@ -47,8 +47,18 @@ impl App {
 	}
 
 	fn handle_cmd(&mut self, input: &str) -> bool {
-		match input {
-			"q" => {
+		let cmd_char = input.chars().last().unwrap_or(' ');
+		let range = &input[..input.len().saturating_sub(1)];
+
+		let mut parts = range.split(',');
+		let start_str = parts.next().unwrap_or("");
+		let stop_str = parts.next().unwrap_or("");
+
+		let start: usize = start_str.parse().unwrap_or(self.current_address);
+		let stop: usize = stop_str.parse().unwrap_or(start);
+
+		match cmd_char {
+			'q' => {
 				if self.modified_buffer {
 					println!("?");
 					// Next line is for the force quit functionality. If the buffer is not saved
@@ -59,20 +69,40 @@ impl App {
 					true
 				}
 			}
-			"a" => {
+			'a' => {
 				self.mode = Mode::Edit;
 				false
 			}
-			"l" => {
-				if self.buffer.len() == 0 {
-					println!("?");
-				} else {
-					for line in &self.buffer {
-						println!("{}", line);
+			'l' => {
+				if start > 0 && stop <= self.buffer.len() && start <= stop {
+					for i in (start - 1)..stop {
+						println!("{}$", self.buffer[i]);
 					}
+					self.current_address = stop;
+				} else {
+					println!("?");
 				}
 
 				false
+			}
+			'd' => {
+				if start > 0 && stop <= self.buffer.len() && start <= stop {
+					self.buffer.drain((start - 1)..stop);
+					self.current_address = if self.buffer.is_empty() {
+						0
+					} else {
+						start.min(self.buffer.len())
+					};
+					self.modified_buffer = true;
+				} else {
+					println!("?");
+					return false;
+				}
+
+				false
+			}
+			'w' => {
+
 			}
 			_ => {
 				println!("?");
@@ -86,7 +116,7 @@ impl App {
 			self.mode = Mode::Cmd;
 		} else {
 			self.buffer.push(input.to_string());
-			self.caret_address = self.buffer.len();
+			self.current_address = self.buffer.len();
 			self.modified_buffer = true;
 		}
 	}
